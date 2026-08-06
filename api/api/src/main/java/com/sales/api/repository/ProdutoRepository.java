0cf5c2b2ae8d9de04cca8574cb343422f34d98a4
ibusquim <igorbusquim2019@gmail.com>
2026-08-06 01:30:02 -0300

    feat: add JPA repositories for all entities

---
+++ api/api/src/main/java/com/sales/api/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.sales.api.repository;
+
+import com.sales.api.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
